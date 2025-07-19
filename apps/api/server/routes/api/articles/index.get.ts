e1803ec50cb202c84ec8288b738d3a0270e14dc2
pierreflw <pg.flauw@gmail.com>
2025-07-19 14:24:20 +0200

    fixs

--- apps/api/server/routes/api/articles/index.get.ts
+++ apps/api/server/routes/api/articles/index.get.ts
@@ -2,113 +2,104 @@ import articleMapper from "~/utils/article.mapper";
 import { definePrivateEventHandler } from "~/auth-event-handler";
 
 export default definePrivateEventHandler(async (event, { auth }) => {
-    const query = getQuery(event);
+  const query = getQuery(event);
 
-    const andQueries = buildFindAllQuery(query, auth);
-    const articlesCount = await usePrisma().article.count({
-        where: {
-            AND: andQueries,
-        },
-    });
+  const andQueries = buildFindAllQuery(query, auth);
+  const articlesCount = await usePrisma().article.count({
+    where: {
+      AND: andQueries,
+    },
+  });
 
-    const articles = await usePrisma().article.findMany({
-        omit: {
-            body: true,
-            updatedAt: true,
-        },
-        where: { AND: andQueries },
+  const articles = await usePrisma().article.findMany({
+    omit: {
+      body: true,
+    },
+    where: { AND: andQueries },
+    orderBy: {
+      createdAt: "desc",
+    },
+    skip: Number(query.offset) || 0,
+    take: Number(query.limit) || 10,
+    include: {
+      tagList: {
         orderBy: {
-            createdAt: 'desc',
+          name: "asc",
         },
-        skip: Number(query.offset) || 0,
-        take: Number(query.limit) || 10,
-        include: {
-            tagList: {
-                select: {
-                    name: true,
-                },
-            },
-            author: {
-                select: {
-                    username: true,
-                    image: true,
-                    followedBy: true,
-                },
-            },
-            favoritedBy: true,
-            _count: {
-                select: {
-                    favoritedBy: true,
-                },
-            },
+        select: {
+          name: true,
         },
-    });
+      },
+      author: {
+        select: {
+          username: true,
+          image: true,
+          followedBy: true,
+        },
+      },
+      favoritedBy: true,
+      _count: {
+        select: {
+          favoritedBy: true,
+        },
+      },
+    },
+  });
 
-    return {
-        articles: articles.map((article: any) => articleMapper(article, auth.id)),
-        articlesCount,
-    };
+  return {
+    articles: articles.map((article: any) => articleMapper(article, auth.id)),
+    articlesCount,
+  };
 }, { requireAuth: false });
 
 const buildFindAllQuery = (query: any, auth: { id: number } | undefined) => {
-    const queries: any = [];
-    const orAuthorQuery = [];
-    const andAuthorQuery = [];
+  const queries: any[] = [];
 
-    orAuthorQuery.push({
-        demo: {
-            equals: true,
+  const orAuthorQuery: any[] = [{ demo: { equals: true } }];
+  if (auth?.id) {
+    orAuthorQuery.push({ id: { equals: auth.id } });
+  }
+
+  const andAuthorQuery: any[] = [];
+  if ("author" in query && query.author) {
+    andAuthorQuery.push({ username: { equals: query.author } });
+  }
+
+  if (orAuthorQuery.length > 0 || andAuthorQuery.length > 0) {
+    const authorConditions: any = {};
+
+    if (orAuthorQuery.length > 0) {
+      authorConditions.OR = orAuthorQuery;
+    }
+
+    if (andAuthorQuery.length > 0) {
+      authorConditions.AND = andAuthorQuery;
+    }
+
+    queries.push({ author: authorConditions });
+  }
+
+  if ("tag" in query && query.tag) {
+    queries.push({
+      tagList: {
+        some: {
+          name: query.tag,
         },
+      },
     });
+  }
 
-    if (auth?.id) {
-        orAuthorQuery.push({
-            id: {
-                equals: auth?.id,
-            },
-        });
-    }
+  if ("favorited" in query && query.favorited) {
+    queries.push({
+      favoritedBy: {
+        some: {
+          username: {
+            equals: query.favorited,
+          },
+        },
+      },
+    });
+  }
 
-    if ('author' in query) {
-        andAuthorQuery.push({
-            username: {
-                equals: query.author,
-            },
-        });
-    }
-
-    if (orAuthorQuery.length > 0 || andAuthorQuery.length > 0) {
-        queries.push({
-            author: {
-                OR: orAuthorQuery,
-                AND: andAuthorQuery,
-            },
-        });
-    }
-
-    queries.push(authorQuery);
-
-    if ('tag' in query) {
-        queries.push({
-            tagList: {
-                some: {
-                    name: query.tag,
-                },
-            },
-        });
-    }
-
-    if ('favorited' in query) {
-        queries.push({
-            favoritedBy: {
-                some: {
-                    username: {
-                        equals: query.favorited,
-                    },
-                },
-            },
-        });
-    }
-
-    return queries;
+  return queries;
 };
